be7ff767e11e0be01dbc2dab8e80ad123fac2217
kanchanasujan <kanchanadevi.sujan2727@gmail.com>
2026-04-26 19:55:38 +0530

    Update

--- roboshop-shell/2_mysql.sh
+++ roboshop-shell/2_mysql.sh
@@ -3,8 +3,8 @@ systemctl enable mysqld
 systemctl start mysqld
 
 mysql -u root -e "
-  CREATE USER 'root'@'%' IDENTIFIED BY 'RoboShop@1';
+  CREATE USER 'root'@'10.0.0.6' IDENTIFIED BY 'RoboShop@1';
   GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-  ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
+  ALTER USER 'root'@'10.0.0.6' IDENTIFIED BY 'RoboShop@1';
   FLUSH PRIVILEGES;
 "
