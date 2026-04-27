4454359b9d1b6ea4f8e22d97e65b1dc00bca3ba0
kanchanasujan <kanchanadevi.sujan2727@gmail.com>
2026-04-27 12:54:58 +0530

    Create folder robo-shell-v1

---
+++ roboshop-shell-v1/4_mongodb.sh
@@ -0,0 +1,9 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+sed -i 's/bindIp: mongod-dev.kanchanadevisujan.online/bindIp: 0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
---
+++ roboshop-shell-v1/linuxcommands.sh
@@ -0,0 +1,19 @@
+sudo yum install git
+git --version
+    git clone https://github.com/kanchanasujan/Devops.git
+cd Devops/roboshop-shell-v1/
+ git pull
+ sudo bash frontend.sh
+
+sudo systemctl status mysqld
+mysql -u root -pRoboShop@1
+
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
+
+mongodb://mongodb:RoboShop@1@<Server_IP_Address>:27017/
+
+
