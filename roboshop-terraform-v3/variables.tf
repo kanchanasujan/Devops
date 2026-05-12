1b21e92adddca528f2f4f05becf5ea44b95a1a9e
kanchanasujan <kanchanadevi.sujan2727@gmail.com>
2026-05-12 21:38:31 +0530

    Update terraform-v3

--- roboshop-terraform-v3/variables.tf
+++ roboshop-terraform-v3/variables.tf
@@ -12,18 +12,18 @@ variable "image_id" {
 
 variable "components" {
   default = {
-    frontend = "Standard_B1s"
-    mysql = "Standard_B1s"
-    valkey = "Standard_B1s"
-    mongodb = "Standard_B1s"
-    rabbitmq = "Standard_B1s"
-    catalogue = "Standard_B1s"
-    user = "Standard_B1s"
-    cart = "Standard_B1s"
-    shipping = "Standard_B1s"
-    order = "Standard_B1s"
-    notification = "Standard_B1s"
-    ratings = "Standard_B1s"
-    payment = "Standard_B1s"
+    frontend = "Standard_B1ms"
+    mysql = "Standard_B1ms"
+    valkey = "Standard_B1ms"
+    mongodb = "Standard_B1ms"
+    rabbitmq = "Standard_B1ms"
+    catalogue = "Standard_B1ms"
+    user = "Standard_B1ms"
+    cart = "Standard_B1ms"
+    shipping = "Standard_B1ms"
+    order = "Standard_B1ms"
+    notification = "Standard_B1ms"
+    ratings = "Standard_B1ms"
+    payment = "Standard_B1ms"
   }
 }
