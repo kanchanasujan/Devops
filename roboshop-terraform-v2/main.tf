3caf144a431efff9688f58f6a30d7d5e6e903257
kanchanasujan <kanchanadevi.sujan2727@gmail.com>
2026-05-13 15:39:43 +0530

    update

--- roboshop-terraform-v2/main.tf
+++ roboshop-terraform-v2/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_network_interface" "main" {
     name                          =  "${each.key}-nic"
     subnet_id                     = "/subscriptions/50b00215-bc86-413d-a70f-7f58601e6267/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vm/subnets/rhel10-vmSubnet"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = [azurerm_public_ip.main[each.key].id]
+    public_ip_address_id          = azurerm_public_ip.main[each.key].id
   }
 }
 
