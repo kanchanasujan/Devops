a82bf249b5a33ebcbca065b4203ae74bd02f6eab
kanchanasujan <kanchanadevi.sujan2727@gmail.com>
2026-05-13 07:49:10 +0530

    update

--- roboshop-ansible-v3/run.sh
+++ roboshop-ansible-v3/run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook  -i $1-dev.kanchanadevisujan.online, roboshop.yml -e component_name=$1 -e ansible_user=devops -e ansible_password=Devops@12345
+ansible-playbook --inventory="${component_name}-dev.kanchanadevisujan.online," roboshop.yml -e component_name="${component_name}" -e ansible_user=devops -e ansible_password=Devops@12345
